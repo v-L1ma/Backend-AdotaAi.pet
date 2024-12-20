383b1e9e2ed4f373e6719f434681453731084c89
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2024-12-19 22:52:02 -0300

    criação da api para o adota ai

---
+++ AdotaAi/server.js
@@ -0,0 +1,62 @@
+import express, { json } from 'express'
+import { PrismaClient } from '@prisma/client'
+import cors from 'cors'
+
+const prisma = new PrismaClient()
+
+const app = express()
+
+app.use(express.json())
+app.use(cors('http://localhost:5173'))
+
+app.get('/animais', async (req,res)=>{
+
+    const animais = await prisma.animal.findMany()
+
+    res.status(200).json(animais)
+})
+
+app.post('/animais', async (req,res)=>{
+
+    await prisma.animal.create({
+        data: {
+            nome: req.body.nome,
+            raca: req.body.raca,
+            datanasc: req.body.datanasc,
+            localizacao: req.body.localizacao
+        }
+    })
+
+    res.status(200).send({msg : 'Animal cadastrado'})
+})
+
+app.put('/animais/:id', async (req,res)=>{
+
+    await prisma.animal.update({
+        where:{
+            id: req.params.id
+        },
+        data: {
+            nome: req.body.nome,
+            raca: req.body.raca,
+            datanasc: req.body.datanasc,
+            localizacao: req.body.localizacao
+        }
+    })
+
+    res.status(200).send({msg : 'Informações atualizadas'})
+
+})
+
+app.delete('/animais/:id', async (req,res)=>{
+
+    await prisma.animal.update({
+        where:{
+            id: req.params.id
+        }
+    })
+    res.status(200).send({msg : 'Animal excluido'})
+})
+
+
+app.listen(3000)
