8d19b6af2de03ac2f3de9e87a844e20f1cbe656b
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-01-15 23:23:26 -0300

    criadas as rotas de upload

--- AdotaAi/routes/private.js
+++ AdotaAi/routes/private.js
@@ -1,17 +1,11 @@
 import express from 'express'
 import { PrismaClient } from '@prisma/client'
 import Multer from "multer";
-import { google } from "googleapis";
-import fs from 'fs'
-import dotenv from 'dotenv';
+import fs from 'fs';
 
 
 const router = express.Router()
-const prisma = new PrismaClient()
-dotenv.config();
-
-const credentials = process.env.credentials
- 
+const prisma = new PrismaClient() 
 
 const multer = Multer({
     storage: Multer.diskStorage({
@@ -19,48 +13,13 @@ const multer = Multer({
         callback(null, `./files`);
       },
       filename: function (req, file, callback) {
-        callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+        callback(null, Date.now() + "_" + file.originalname);
       },
     }),
     limits: {
       fileSize: 5 * 1024 * 1024,
     },
   });
-
-  const authenticateGoogle = () => {
-    const auth = new google.auth.GoogleAuth({
-      credentials,
-      scopes: "https://www.googleapis.com/auth/drive",
-    });
-    return auth;
-  };
-
-  const uploadToGoogleDrive = async (file, auth) => {
-    const fileMetadata = {
-      name: file.originalname,
-      parents: ["1I0maE0nQ42N_ucaN76AK4VUdbOsU1ebH"], // Change it according to your desired parent folder id
-    };
-  
-    const media = {
-      mimeType: file.mimetype,
-      body: fs.createReadStream(file.path),
-    };
-  
-    const driveService = google.drive({ version: "v3", auth });
-  
-    const response = await driveService.files.create({
-      requestBody: fileMetadata,
-      media: media,
-      fields: "id",
-    });
-    return response.data.id;
-  };
-
-  const deleteFile = (filePath) => {
-    fs.unlink(filePath, () => {
-      console.log("file deleted");
-    });
-  };
 
 
 router.get('/animais', async (req,res)=>{
@@ -85,6 +44,7 @@ router.get('/animais/:id', async (req,res)=>{
     res.status(200).json(animais)
 })
 
+
 router.post('/animais', multer.single('file'), async (req, res) => {
     try {
   
@@ -93,14 +53,10 @@ router.post('/animais', multer.single('file'), async (req, res) => {
         return res.status(400).send('Nenhum arquivo enviado.');
       }
   
-      const auth = authenticateGoogle();
-      const uploadResponse = await uploadToGoogleDrive(req.file, auth);
-  
-      const pictureUrl = uploadResponse; // Assuma que isso é a URL ou ID do arquivo
-  
       // Salvar no banco
       await prisma.animal.create({
         data: {
+            idDono: req.body.idDono,
             nome: req.body.nome,
             raca: req.body.raca,
             foto: req.body.foto,
@@ -110,11 +66,9 @@ router.post('/animais', multer.single('file'), async (req, res) => {
             castrado: req.body.castrado,
             vermifugado: req.body.vermifugado,
             descricao: req.body.descricao,
-            Picture: pictureUrl, // Salva a URL da imagem
+            Picture: req.file.filename,
         },
       });
-
-      deleteFile(req.file.path);
   
       res.status(200).send({msg : 'Animal cadastrado'})
     } catch (error) {
@@ -158,12 +112,33 @@ router.put('/animais/:id', async (req,res)=>{
             castrado: req.body.castrado,
             vermifugado: req.body.vermifugado,
             descricao: req.body.descricao,
+            Picture: req.file.path,
         },
     })
 
     res.status(200).send({msg : 'Informações atualizadas'})
 
 })
+
+//Carregar animais cadastrados pelo dono
+router.get('/animais-cadastrados', async (req, res) => {
+  try {
+      const { id } = req.query; // esse id é o do dono do pet
+
+      if (!id) {
+          return res.status(400).json({ message: "Id é obrigatório" });
+      }
+
+      const animais = await prisma.animal.findMany({
+          where: { idDono: id }, // Certifique-se do tipo do campo
+      });
+
+      res.status(200).json(animais);
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Erro no lado do servidor" });
+  }
+});
 
 router.delete('/animais/:id', async (req,res)=>{
 
--- AdotaAi/routes/public.js
+++ AdotaAi/routes/public.js
@@ -3,29 +3,12 @@ import bcrypt from 'bcrypt'
 import { PrismaClient } from '@prisma/client'
 import jwt from 'jsonwebtoken'
 import Multer from "multer";
-import { google } from "googleapis";
 import fs from 'fs'
 
 const prisma = new PrismaClient()
 const router = express.Router()
 
 const JWT_SECRET = process.env.JWT_SECRET
-
-const credentials = 
-  {
-    "type": "service_account",
-    "project_id": "adotaai-447500",
-    "private_key_id": "da20436fea23658ea407d4761b9066f670800adb",
-    "private_key": process.env.private_key.replace(/\\n/g, '\n'),
-    "client_email": "adotaai@adotaai-447500.iam.gserviceaccount.com",
-    "client_id": "110718253060481934072",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/adotaai%40adotaai-447500.iam.gserviceaccount.com",
-    "universe_domain": "googleapis.com",
-  }
-
 
 
 const multer = Multer({
@@ -34,49 +17,13 @@ const multer = Multer({
         callback(null, `./files`);
       },
       filename: function (req, file, callback) {
-        callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+        callback(null, Date.now() + "_" + file.originalname);
       },
     }),
     limits: {
       fileSize: 5 * 1024 * 1024,
     },
   });
-
-  const authenticateGoogle = () => {
-    const auth = new google.auth.GoogleAuth({
-    
-      credentials,
-      scopes: "https://www.googleapis.com/auth/drive",
-    });
-    return auth;
-  };
-
-  const uploadToGoogleDrive = async (file, auth) => {
-    const fileMetadata = {
-      name: file.originalname,
-      parents: ["1ABftdbXNw2MvdnEAglxLsjvCRxLpPLKD"], // Change it according to your desired parent folder id
-    };
-  
-    const media = {
-      mimeType: file.mimetype,
-      body: fs.createReadStream(file.path),
-    };
-  
-    const driveService = google.drive({ version: "v3", auth });
-  
-    const response = await driveService.files.create({
-      requestBody: fileMetadata,
-      media: media,
-      fields: "id",
-    });
-    return response.data.id;
-  };
-
-  const deleteFile = (filePath) => {
-    fs.unlink(filePath, () => {
-      console.log("file deleted");
-    });
-  };
 
   router.post('/cadastro', multer.single('file'), async (req, res) => {
     try {
@@ -86,11 +33,6 @@ const multer = Multer({
       if (!req.file) {
         return res.status(400).send('Nenhum arquivo enviado.');
       }
-  
-      const auth = authenticateGoogle();
-      const uploadResponse = await uploadToGoogleDrive(req.file, auth);
-  
-      const pictureUrl = uploadResponse; // Assuma que isso é a URL ou ID do arquivo
   
       // Criptografar a senha
       const salt = await bcrypt.genSalt(10);
@@ -105,11 +47,9 @@ const multer = Multer({
           cpf,
           birthdate,
           phone,
-          Picture: pictureUrl, // Salva a URL da imagem
+          Picture: req.file.filename,
         },
       });
-
-      deleteFile(req.file.path);
   
       res.status(200).json({ msg: 'Usuário criado com sucesso!' });
     } catch (error) {
@@ -117,6 +57,9 @@ const multer = Multer({
       res.status(500).json({ message: 'Erro no servidor' });
     }
   });
+
+router.use('/ver', express.static("files"))
+//http://localhost:3000/ver/file.path
 
 router.post('/login', async (req,res)=>{
     try {
@@ -143,7 +86,6 @@ router.post('/login', async (req,res)=>{
         const userWithoutPassword = {...user}
         delete userWithoutPassword.password
         res.status(200).json({"user":userWithoutPassword , "token":token})
-        
 
     } catch (error) {
         console.error(error)
